cff42255b0465ff9133daa2c638601fadeadf663
m6hdix <m6hdixx@gmail.com>
2023-06-06 08:10:46 +0430

    ✅ :compelet hooks client pagination

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+const url ="https://react-mini-projects-api.classbon.com/Programmer/programmers"
+function App() {
+
+  return (
+  )
+}
+
+export default App;
---
+++ src/hooks/usePagination.jsx
@@ -0,0 +1,20 @@
+import _ from "lodash";
+import { useEffect, useState } from "react";
+
+const usePagination = ({ url, pageSize }) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const paginateData = _.chunk(data, pageSize);
+    setData(paginateData);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return [data, loading];
+};
+
+export default usePagination;
